b0c438ca36e0a70e4cbf23b492508315671fd7d9
im-mde <matthewdustin12@gmail.com>
2021-01-10 22:54:06 -0600

    Added valid.py to document valid api resource values

---
+++ aioyoutube/valid.py
@@ -0,0 +1,23 @@
+RATINGS = frozenset({'like', 'dislike', 'none'})
+KINDS = frozenset({
+  'watermark',  
+  'videoCategory',
+  'video',
+  'videoAbuseReportReason',
+  'thumbnail',
+  'subscription',
+  'search',
+  'playlist',
+  'playlistItem',
+  'membershipLevel',
+  'member',
+  'i18nRegion',
+  '118nLanguage',
+  'commentThread',
+  'comment',
+  'channelSection',
+  'channel',
+  'channelBanner',
+  'caption',
+  'activity'
+})
