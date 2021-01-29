e793e66e63cbd39d6c71b704f68cf634a628bd7c
im-mde <matthewdustin12@gmail.com>
2021-01-28 23:28:44 -0600

    Added comments to parse.py

--- aioyoutube/parse.py
+++ aioyoutube/parse.py
@@ -9,21 +9,28 @@ def parse_resource(resource: str):
     if resource_ not in YOUTUBE_RESOURCES:
         raise ValueError('Kind argument must be one of: %r' % list(YOUTUBE_RESOURCES))
     
-    if resource_[-1] == 'y': 
+    # "search" resource is a special case that doesn't convert to plural
+    # ex. video -> videos but search -> search
+
+    if resource_ == 'search': 
+        return 'search'
+    elif resource[-1] == 'y': 
         return resource_[0:-1] + 'ies'
-    elif resource_ != 'search': 
-        return resource_ + 's'
     else: 
         return resource_
 
 def build_endpoint(query_type: str, key: str, part: list = [], **kwargs):
     
     endpoint = '{}?'.format(query_type)
+
     if len(part) > 0:
         part = ','.join(part)
         endpoint = '{}?part={}'.format(query_type, part)
-    
+
     for key_, value in kwargs.items():
         endpoint += '&{}={}'.format(key_, str(value))
     
+    # example of a generated endpoint:
+    # ex. videos?part=snippet&id=dQw4w9WgXcQ&maxResults=50&key=[YOUR_API_KEY]
+
     return endpoint + '&key={}'.format(key)
