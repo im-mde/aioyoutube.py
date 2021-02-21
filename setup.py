b6f1743f5d0b227da86e5480eb12b4f0c88300f3
im-mde <matthewdustin12@gmail.com>
2021-02-20 19:58:40 -0600

    Added setup.py for building package and added .gitignore entries

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+AUTHOR = 'im-mde'
+VERSION = '0.5.0'
+DESCRIPTION = 'An asynchronous Python wrapper for the YouTube Data API'
+
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+long_description = ''
+with open('README.MD') as f:
+    long_description = f.read()
+
+setup(
+    name='aioyoutube.py',
+    author=AUTHOR,
+    url='https://github.com/im-mde/aioyoutube.py',
+    version=VERSION,
+    packages=['aioyoutube'],
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    install_requires=requirements,
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Topic :: Internet',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Utilities',
+    ]
+)
