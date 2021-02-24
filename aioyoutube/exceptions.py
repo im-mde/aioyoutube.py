e9b4991ad78d416500ceab3253ada85ce1e4fcd4
im-mde <matthewdustin12@gmail.com>
2021-02-23 20:55:03 -0600

    Changed exceptions inheritence structure and added new exceptions

--- aioyoutube/exceptions.py
+++ aioyoutube/exceptions.py
@@ -16,7 +16,25 @@ async def find_http_exception(response: YouTubeAPIResponse):
     else: return
 
 
-class YouTubeAPIHttpException(Exception):
+class YouTubeAPIException(Exception):
+
+    """
+        Parent for all exceptions in this library.
+
+        This exception should never be raised directly.
+
+        Parent(s):
+            Exception
+        
+        Attribute(s):
+            message type(str): message sent from child exception
+    """
+
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class YouTubeAPIHttpException(YouTubeAPIException):
 
     """
         Generic HTTP exception for the YouTube Data API.
@@ -25,7 +43,7 @@ class YouTubeAPIHttpException(Exception):
         equal to 400 and is not corresponding with one of its child exceptions.
 
         Parent(s):
-            Exception
+            YouTubeAPIException
         
         Attribute(s):
             code type(int): status code of http response
@@ -33,7 +51,8 @@ class YouTubeAPIHttpException(Exception):
     """
 
     def __init__(self, code: int, json: dict):
-        self.message = 'Status Code {}: {}'.format(str(code), json['error']['message'])
+        
+        self.message = 'Status Code {}: {}.'.format(str(code), json['error']['message'])
         super().__init__(self.message)
 
 
@@ -118,7 +137,26 @@ class YouTubeForbiddenException(YouTubeAPIHttpException):
         super().__init__(code, json)
 
 
-class YouTubeResourceInvalidException(Exception):
+class YouTubeValueInvalidException(YouTubeAPIException):
+
+    """
+        Generic invalid YouTube argument exception.
+
+        This exception represents the scenario where an invalid argument value
+        is entered in a client class. 
+
+        Parent(s):
+            YouTubeAPIException
+
+        Attribute(s):
+            message type(str): message sent from child exception or manually initialized.
+    """
+
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class YouTubeResourceInvalidException(YouTubeValueInvalidException):
 
     """
         Invalid YouTube resource exception.
@@ -127,18 +165,19 @@ class YouTubeResourceInvalidException(Exception):
         the resource argument of a client method.
 
         Parent(s):
-            Exception
+            YouTubeValueInvalidException
 
         Attribute(s):
             None
     """
 
     def __init__(self):
-        self.message = 'Resource argument must be one of: {}'.format(get_youtube_resources())
+        
+        self.message = 'Resource argument must be one of: {}.'.format(get_youtube_resources())
         super().__init__(self.message)
 
 
-class YouTubeRatingInvalidException(Exception):
+class YouTubeRatingInvalidException(YouTubeValueInvalidException):
 
     """
         Invalid YouTube rating exception.
@@ -147,12 +186,74 @@ class YouTubeRatingInvalidException(Exception):
         into the rating argument of the "rate" method of the client.
 
         Parent(s):
-            Exception
+            YouTubeValueInvalidException
 
         Attribute(s):
             None
     """
 
     def __init__(self):
-        self.message = 'Rating argument must be one of: {}'.format(get_ratings())
+        
+        self.message = 'Rating argument must be one of: {}.'.format(get_ratings())
+        super().__init__(self.message)
+
+
+class YouTubeNoneValueException(YouTubeAPIException):
+
+    """
+        Generic None value exception.
+
+        This exception represents the scenario where a None value was input
+        for a required attribute. 
+
+        Parent(s):
+            YouTubeAPIException
+
+        Attribute(s):
+            None
+    """
+
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class YouTubeKeyNoneException(YouTubeNoneValueException):
+
+    """
+        YouTube key is detected to be set to None. 
+
+        This exception occurs when the key is set to None instead
+        of a string.
+
+        Parent(s):
+            YouTubeNoneValueException
+
+        Attribute(s):
+            None
+    """
+
+    def __init__(self):
+
+        self.message = 'YouTube API key is set to "None".'
+        super().__init__(self.message)
+
+
+class YouTubeTokenNoneException(YouTubeNoneValueException):
+
+    """
+        Oauth token is detected to be set to None. 
+
+        This exception occurs when the OAuth token is set to None instead
+        of a string.
+
+        Parent(s):
+            YouTubeNoneValueException
+
+        Attribute(s):
+            None
+    """
+
+    def __init__(self):
+
+        self.message = 'OAuth token is set to "None".'
         super().__init__(self.message)
